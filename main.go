ba197e37fb1e59d0aa965054632be66fa025d35f
Radu Ioan Fericean <radu@fericean.ro>
2017-11-13 21:25:38 +0200

    started server mode

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"net/http"
 	"os"
 
 	"github.com/rif/go-eventsocket/eventsocket"
@@ -11,39 +12,37 @@ import (
 )
 
 var (
-	host = flag.String("host", "localhost", "freeswitch host address")
-	port = flag.Int("port", 8021, "freeswitch port")
-	pass = flag.String("passs", "ClueCon", "freeswitch password")
+	host           = flag.String("host", "localhost", "freeswitch host address")
+	port           = flag.Int("port", 8021, "freeswitch port")
+	pass           = flag.String("pass", "ClueCon", "freeswitch password")
+	serve          = flag.Bool("serve", false, "run as a server")
+	listen_address = flag.String("listen_address", "127.0.0.1", "listen on address")
+	listen_port    = flag.Int("listen_port", 9191, "listen on port")
 )
 
-func main() {
-	flag.Parse()
-	l := log.New(os.Stderr, "", 0)
-	conn, err := eventsocket.Dial(fmt.Sprintf("%s:%d", *host, *port), *pass)
+func getData(conn *eventsocket.Connection) (sessions *utils.Sessions, sofiaProfiles []*utils.SofiaProfile, err error) {
+	ev, err := conn.Send(`api json {"command" : "status", "data" : ""}`)
 	if err != nil {
-		l.Print("error connecting to fs: ", err)
+		return nil, nil, fmt.Errorf("error sending status command: %v", err)
 	}
-	var sessions utils.Sessions
-	if ev, err := conn.Send(`api json {"command" : "status", "data" : ""}`); err != nil {
-		l.Print("error sending status command: ", err)
-	} else {
-		c, err := utils.LoadStatusJSON(ev.Body)
-		if err != nil || c.Status != "success" {
-			l.Printf("error parsing status command: %v %+v", err, c)
-		} else {
-			sessions = c.Response.Sessions
-		}
+	c, err := utils.LoadStatusJSON(ev.Body)
+	if err != nil || c.Status != "success" {
+		return nil, nil, fmt.Errorf("error parsing status command: %v %+v", err, c)
 	}
-	var sofiaProfiles []*utils.SofiaProfile
-	if ev, err := conn.Send("api sofia xmlstatus"); err != nil {
-		l.Print("error sending xmlstatus: ", err)
-	} else {
-		sofiaProfiles, err = utils.ParseSofiaStatus(ev.Body)
-		if err != nil {
-			l.Print("error parsing xmlstatus: ", err)
-		}
+	sessions = &c.Response.Sessions
+	ev, err = conn.Send("api sofia xmlstatus")
+	if err != nil {
+		return nil, nil, fmt.Errorf("error sending xmlstatus: %v", err)
 	}
-	fmt.Printf("freeswitch_sessions active=%d,peak=%d,peak_5min=%d,total=%d,rate_current=%d,rate_max=%d,rate_peak=%d,rate_peak_5min=%d\n",
+	sofiaProfiles, err = utils.ParseSofiaStatus(ev.Body)
+	if err != nil {
+		return nil, nil, fmt.Errorf("error parsing xmlstatus: %v", err)
+	}
+	return
+}
+
+func getOutput(sessions *utils.Sessions, sofiaProfiles []*utils.SofiaProfile) string {
+	out := fmt.Sprintf("freeswitch_sessions active=%d,peak=%d,peak_5min=%d,total=%d,rate_current=%d,rate_max=%d,rate_peak=%d,rate_peak_5min=%d\n",
 		sessions.Count.Active,
 		sessions.Count.Peak,
 		sessions.Count.Peak5min,
@@ -54,9 +53,38 @@ func main() {
 		sessions.Rate.Peak5min,
 	)
 	for _, sofiaProfile := range sofiaProfiles {
-		fmt.Printf("freeswitch_profile_sessions,profile=%s,ip=%s running=%s\n",
+		out += fmt.Sprintf("freeswitch_profile_sessions,profile=%s,ip=%s running=%s\n",
 			sofiaProfile.Name,
 			sofiaProfile.Address,
 			sofiaProfile.Running)
 	}
+	return out
+}
+
+func main() {
+	flag.Parse()
+	l := log.New(os.Stderr, "", 0)
+	conn, err := eventsocket.Dial(fmt.Sprintf("%s:%d", *host, *port), *pass)
+	if err != nil {
+		l.Print("error connecting to fs: ", err)
+	}
+	defer conn.Close()
+	if !*serve {
+		sessions, sofiaProfiles, err := getData(conn)
+		if err != nil {
+			l.Print(err.Error())
+		}
+		fmt.Print(getOutput(sessions, sofiaProfiles))
+		os.Exit(0)
+	}
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		sessions, sofiaProfiles, err := getData(conn)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		w.Write([]byte(getOutput(sessions, sofiaProfiles)))
+	})
+	log.Fatal(http.ListenAndServe(fmt.Sprintf("%s:%d", *listen_address, *listen_port), nil))
 }
